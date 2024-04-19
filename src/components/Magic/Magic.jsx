8fc1d7414ea0b96303421b7e8c8719be48bfda2c
Jihad Hasan <mdjihat0021@gmail.com>
2024-04-19 20:11:34 +0600

    finished magic section responsiveness

--- src/components/Magic/Magic.jsx
+++ src/components/Magic/Magic.jsx
@@ -11,17 +11,17 @@ import strong  from "../../assets/strong 1.png"
 const Magic = () => {
   return (
     <div className="py-12 bg-white">
-      <div className="flex flex-col items-center space-y-5">
-        <h1 className="text-[#3A643B] text-center font-bold text-3xl border-b-4 w-[536px] border-[#C3D0C4]">
+      <div className="flex flex-col items-center space-y-5 px-1">
+        <h1 className="text-[#3A643B] text-center font-bold text-xl md:text-3xl border-b-4 max-w-[536px] border-[#C3D0C4]">
           Discover Ayurveda’s Magic With Us
         </h1>
-        <p className="text-[#666666] text-center w-[536px]">
+        <p className="text-[#666666] text-center max-w-[536px]">
           Ayurvedic treatment aims to balance your body and mind, bringing
           harmony and vitality. It's like a journey to better health using
           ancient wisdom, a totally effective approach for a better life.{" "}
         </p>
       </div>
-      <div className="flex items-center justify-center text-center relative mt-10">
+      <div className="md:flex items-center justify-center text-center relative mt-10 hidden">
         <div className=" flex flex-col gap-16 ">
         <div className="flex items-center gap-4 absolute top-10 left-28">
           <div className="text-right w-56 ">
@@ -112,6 +112,99 @@ const Magic = () => {
           
         </div>
         </div>
+      </div>
+      <div className="flex flex-col md:hidden">
+      <div
+          className="object-cover bg-center bg-cover px-6 mt-6"
+          style={{
+            backgroundImage: `url(${bgimage})`,
+          }}
+        >
+          <img className="pt-10" src={image57} alt="" />
+        </div>
+        <div className=" grid grid-cols-2 gap-2 mx-2">
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={heartbeat} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Personalized Wellness</h2>
+            <p className="text-[10px] text-[#515151]">
+              Get treatments made just for you based on your individual doshas
+              (body type)
+            </p>
+          </div>
+         
+        </div>
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={ribbon} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Focus on prevention</h2>
+            <p className="text-[10px] text-[#515151]">
+            Stop problems even before they start.
+            </p>
+          </div>
+         
+        </div>
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={lotusposition} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Mind-Body Connection</h2>
+            <p className="text-[10px] text-[#515151]">
+            Keep your mind and body in sync for a happy life.
+            </p>
+          </div>
+         
+        </div>
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={protection} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Holistic Healing</h2>
+            <p className="text-[10px] text-[#515151]">
+            Fix the root problem for long-lasting health.
+            </p>
+          </div>
+         
+        </div>
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={ayurveda} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Natural Remedies</h2>
+            <p className="text-[10px] text-[#515151]">
+            Using herbs and natural therapies for healing.
+            </p>
+          </div>
+         
+        </div>
+        <div className="flex flex-col border rounded-lg items-center gap-1 ">
+        <div className="bg-[#EDF5FA] rounded-full  p-2 mt-1">
+            {" "}
+            <img className="w-8" src={strong} alt="" />
+          </div>
+          <div className="text-center  px-2 pb-3">
+            <h2 className="text-[12px] font-bold">Boosting immunity</h2>
+            <p className="text-[10px] text-[#515151]">
+            Stay strong and healthy for life, not just for today.
+            </p>
+          </div>
+         
+        </div>
+       
+        </div>
+       
       </div>
     </div>
   );
