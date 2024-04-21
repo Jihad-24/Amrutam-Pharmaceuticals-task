69c05bd33545325deb323f4c93c2e1efa79b4da3
Jihad Hasan <mdjihat0021@gmail.com>
2024-04-21 23:42:14 +0600

    set website max width

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Approach from "./components/approach/Approach";
 
 function App() {
   return (
-    <div className="max-w-[1350px] mx-auto">
+    <div className="max-w-[1340px] mx-auto">
       <Banner />
       <Magic />
       <Consultations />
